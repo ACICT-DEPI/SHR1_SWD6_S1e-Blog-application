81329e0a11f3bcb5bba34f3f9021e376ab6d1cef
Osama-Elwan <osama.elwan01@gmail.com>
2024-10-19 12:45:24 +0300

    add @auth and @guest

--- resources/views/layouts/site/master.blade.php
+++ resources/views/layouts/site/master.blade.php
@@ -2,10 +2,20 @@
 <html lang="en">
 @include('layouts.site.head')
 <body>
+    @guest
+
     <div class="login-register-container">
         <a href="{{ url('/login/form') }}" class="login-link">Login</a>
         <a href="{{ url('/registration/form') }}" class="register-link">Register</a>
       </div>
+      @endguest
+      @auth
+      <div class="login-register-container">
+          <a href="{{ url('/my/posts') }}" class="login-link">Dashboard</a>
+
+        </div>
+
+    @endauth
 	@include('layouts.site.main-header')
     <div class="container-fluid">
         <main class="tm-main">
